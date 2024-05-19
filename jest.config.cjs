0ac5ae187ccd8b8d8e52ce41471c538cfb9c44af
jinhyeong.kim@snetsystems.co.kr <jinhyeong.kim@snetsystems.co.kr>
2024-05-19 17:53:11 +0900

    test: add test code for update-readme

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,26 +1,14 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        diagnostics: {
-          ignoreCodes: [1343],
-        },
-        astTransformers: {
-          before: [
-            {
-              path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
-              options: {
-                metaObjectReplacement: { url: 'https://www.url.com' },
-              },
-            },
-          ],
-        },
+        tsconfig: 'tsconfig.json',
+        useESM: true,
       },
     ],
   },
-  testMatch: ['**/src/scripts/**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
 };
